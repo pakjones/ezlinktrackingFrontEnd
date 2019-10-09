e6dde537a956549f7540e32c492115bc012b197b
Richard <richl.laconte@gmail.com>
2019-10-09 12:40:28 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -115,7 +115,17 @@ class App extends React.Component {
             </div>
             <div className="col-lg-4">
               <div>
-
+              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+              
+              <ins class="adsbygoogle"
+                  style="display:block"
+                  data-ad-client="ca-pub-8669362298748043"
+                  data-ad-slot="8051362172"
+                  data-ad-format="auto"
+                  data-full-width-responsive="true"></ins>
+              <script>
+                  (adsbygoogle = window.adsbygoogle || []).push({});
+              </script>
               </div>
             </div>
           </div>
